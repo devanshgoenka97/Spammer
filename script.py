6dc99d3f342a769d73747816c0045265517885e6
Devansh Goenka <devanshgoenka97@gmail.com>
2016-09-03 18:54:39 +0530

    update

--- script.py
+++ script.py
@@ -5,12 +5,12 @@ import requests
 
 try :
  i=0
- url = "https://docs.google.com/forms/d/e/1FAIpQLScENJsiop7yKEnS3-Wv2dFA7xxXOp1YA46922L9-ORSxBRCNQ/formResponse"
+ url = "https://docs.google.com/forms/d/e/1FAIpQLScp8GLVOGeJM2Lq1iYjUO-2gD0PxCtE-t8zQ7JuTmDVcPeaSg/formResponse"
  user_agent = {
-	'Referer'   : 'https://docs.google.com/forms/d/e/1FAIpQLScENJsiop7yKEnS3-Wv2dFA7xxXOp1YA46922L9-ORSxBRCNQ/viewform',
+	'Referer'   : 'https://docs.google.com/forms/d/e/1FAIpQLScp8GLVOGeJM2Lq1iYjUO-2gD0PxCtE-t8zQ7JuTmDVcPeaSg/viewform?c=0&w=1',
 	'User-Agent' : 'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36'}
  while 1:
-	 spam_data = {"entry.410813173"  : "Yes","entry.1269816401" : "10","entry.1963899682" : "560","entry.1616294512" : "10"}
+	 spam_data = {"entry.2056656144"  : "HEY HEY"}
 	 r=requests.post(url,data=spam_data,headers=user_agent)
 	 i=i+1
 	 if(i%3==0):
